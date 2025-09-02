3a8b92148bb1fe4c96d9710e479e0ca8c6ed9ac6
ani8727 <aniketgupta.8727@gmail.com>
2025-09-02 11:32:09 +0530

    four appraoch majority element

--- Array/MajorityElement.cpp
+++ Array/MajorityElement.cpp
@@ -1,3 +1,5 @@
+// 169. Majority Element - four different approach
+
 #include<iostream>
 #include<unordered_map>
 #include<vector>
@@ -23,26 +25,26 @@ using namespace std;
 
 // 2. Optimal apprach - t.c =nlogn
 
-int majority(vector<int> nums){
-    sort(nums.begin(),nums.end());
-    int n=nums.size();
-    int freq=1;
-    int ans=nums[0];
-    for(int i=1;i<n;i++){
-        if(nums[i]==nums[i-1]){
-           freq++;
-        }else{
-            freq=1;
-            ans=nums[i];
-        }
-       if(freq>n/2){
-        return ans;
-       }
-    }
-   return ans;
-}
+// int majority(vector<int> nums){
+//     sort(nums.begin(),nums.end());
+//     int n=nums.size();
+//     int freq=1;
+//     int ans=nums[0];
+//     for(int i=1;i<n;i++){
+//         if(nums[i]==nums[i-1]){
+//            freq++;
+//         }else{
+//             freq=1;
+//             ans=nums[i];
+//         }
+//        if(freq>n/2){
+//         return ans;
+//        }
+//     }
+//    return ans;
+// }
 
-// 3. better appraoch
+// 3. better appraoch - unorder_map
 
 // int majority(vector<int> nums){
 //    int n=nums.size();
@@ -60,6 +62,33 @@ int majority(vector<int> nums){
 //    return -1;
 // }
 
+// 4. optimal appraoch - moore's voting algorithm
+
+int majority(vector<int> nums){
+    int n=nums.size();
+    int freq=0, ans=0;
+    for(int i=0; i<n; i++){
+        if(freq==0){
+            ans=nums[i];
+        }
+        if(ans==nums[i]){
+            freq++;
+        }else {
+            freq--;
+        }
+    }
+    freq=0;
+    for(int val:nums){
+        if(val==ans){
+            freq++;
+        }
+    }
+    if(freq>n/2){
+        return ans;
+    }else {
+        return -1;
+    }
+}
 int main(){
     vector<int> nums={1,2,3,2,5,4,6,2,2,2,2,};
     cout<<majority(nums)<<endl;
